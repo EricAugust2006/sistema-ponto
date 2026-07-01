c15aa81b71e5ec13e5eb49f0452f41e470d7f38e
Eric Augusto <149190070+EricAugust2006@users.noreply.github.com>
2026-07-01 02:40:01 +0000

    Preparando arquivos iniciais/basicos para o projeto

---
+++ sistema-ponto/app/api/v1/status/index.ts
@@ -0,0 +1,5 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function Status(req: NextRequest, res: NextResponse) {
+  // codigo para pedir status da pagina
+}
---
+++ sistema-ponto/infra/database.js
@@ -0,0 +1,31 @@
+import { Client } from "pg";
+import "dotenv/config";
+
+async function query(queryObject) {
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client?.end();
+  }
+}
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+  });
+
+  await client.connect();
+  return client;
+}
+
+export default { query };
