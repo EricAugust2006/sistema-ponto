d4741d3d6e925860e76d59c9d250718a1ac9f477
Eric Augusto <149190070+EricAugust2006@users.noreply.github.com>
2026-07-29 08:47:18 -0300

    part1: preparing route ponto for empregados (#7)
    * part1: preparing route ponto for empregados
    
    * fixing error in
    
    * fixing error in  - changed  for

---
+++ sistema-ponto/_infra/auth.ts
@@ -0,0 +1,32 @@
+import database from "@/_infra/database";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function autenticatorRequisicao(req: NextRequest) {
+  const tokenCookie = req.cookies.get("session_token");
+
+  if (!tokenCookie) {
+    return null;
+  }
+
+  const token = tokenCookie.value;
+  try {
+    const res = await database.query({
+      text: `
+        SELECT empregados.id, empregados.nome, empregados.email, empregados.matricula
+        FROM sessoes
+        JOIN empregados ON empregados.id = sessoes.empregado_id
+        WHERE sessoes.token = $1 AND sessoes.expira_em now()
+      `,
+      values: [token],
+    });
+
+    if (res.rowCount === 0) {
+      return null;
+    }
+
+    return res.rows[0];
+  } catch (err) {
+    console.error("Erro ao autenticar requisição", err);
+    return null;
+  }
+}
