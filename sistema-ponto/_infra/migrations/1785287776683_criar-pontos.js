3c56100bc5b7ef54b9d2cd41207c2ec26ccfa082
Eric Augusto <149190070+EricAugust2006@users.noreply.github.com>
2026-07-29 08:47:18 -0300

    part1: preparing route ponto for empregados (#7)
    * part1: preparing route ponto for empregados
    
    * fixing error in
    
    * fixing error in  - changed  for

---
+++ sistema-ponto/_infra/migrations/1785287776683_criar-pontos.js
@@ -0,0 +1,38 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable("pontos", {
+    id: { type: "bigserial", primaryKey: true },
+    empregado_id: {
+      type: "integer",
+      notNull: true,
+      references: "empregados",
+      onDelete: "CASCADE",
+    },
+    tipo: {
+      type: "text",
+      notNull: true,
+      check: "tipo IN ('entrada', 'saida_almoco', 'retorno_almoco', 'saida')",
+    },
+    criado_em: { type: "timestamp", notNull: true, default: pgm.func("now()") },
+  });
+
+  pgm.createIndex("pontos", "empregado_id");
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable("pontos");
+};
