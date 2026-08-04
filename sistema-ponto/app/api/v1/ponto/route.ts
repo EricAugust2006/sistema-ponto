67e20d151681efa4df701a5079d4b29b8f14814d
Eric Augusto <149190070+EricAugust2006@users.noreply.github.com>
2026-08-03 23:07:49 -0300

    Feature/rota ponto (#8)
    * part1: preparing route ponto for empregados
    
    * fixing error in
    
    * fixing error in  - changed  for
    
    * adiciona endpoint POST /api/v1/ponto com autenticação
    
    * atualizando

---
+++ sistema-ponto/app/api/v1/ponto/route.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from "next/server";
+import { autenticatorRequisicao } from "@/_infra/auth";
+import database from "@/_infra/database.js";
+import z from "zod";
+
+export async function POST(req: NextRequest) {
+  const empregado = await autenticatorRequisicao(req);
+
+  if (!empregado) {
+    return NextResponse.json({ erro: "Não Autorizado" }, { status: 401 });
+  }
+
+  try {
+    //pegar o tipo do ponto
+    const rawBody = await req.json();
+
+    const criarPontoSchema = z.object({
+      type: z.enum(["entrada", "saida_almoco", "retorno_almoco", "saida"], {
+        message: "Tipo de ponto inválido",
+      }),
+    });
+
+    type CriarPonto = z.infer<typeof criarPontoSchema>;
+
+    const body = criarPontoSchema.parse(rawBody);
+
+    const res = await database.query({
+      text: `
+      INSERT INTO pontos (empregado_id, tipo)
+      VALUES ($1, $2)
+      RETURNING *`,
+      values: [empregado.id, body.type],
+    });
+
+    return NextResponse.json(res.rows[0], { status: 201 });
+  } catch (err) {
+    return NextResponse.json({ error: "Dados inválidos" }, { status: 400 });
+  }
+}
