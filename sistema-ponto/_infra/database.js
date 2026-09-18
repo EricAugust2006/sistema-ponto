d9f1d5cc4259aa279138a233d369cef511afd411
Eric Augusto <149190070+EricAugust2006@users.noreply.github.com>
2026-09-18 12:55:59 +0000

    fix PostgreSQL SSL connection

--- sistema-ponto/_infra/database.js
+++ sistema-ponto/_infra/database.js
@@ -2,15 +2,10 @@ import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  allowExitOnIdle: true,
   ssl: {
     rejectUnauthorized: false,
   },
+  allowExitOnIdle: true,
 });
 
 async function query(queryObject) {
